0b639f8599c3cc96034aaa5b283aa21ffb71a8a6
Adrijana <adrijana.jovicic@gmail.com>
2020-05-06 12:44:35 +0200

    SearchAdapter

--- app/src/main/java/com/example/a8filmovizapogledati/activities/MainActivity.java
+++ app/src/main/java/com/example/a8filmovizapogledati/activities/MainActivity.java
@@ -3,9 +3,15 @@ package com.example.a8filmovizapogledati.activities;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.example.a8filmovizapogledati.R;
 
@@ -13,12 +19,19 @@ public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
 
+    private ImageButton btnSearch;
+    private EditText movieName;
+    private RecyclerView recyclerView;
+
+    int position = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
 
         setupToolbar();
+        inicijalizacija();
     }
 
     public void setupToolbar() {
@@ -35,5 +48,27 @@ public class MainActivity extends AppCompatActivity {
             actionBar.setHomeButtonEnabled( true );
             actionBar.show();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate( R.menu.menu, menu );
+        return super.onCreateOptionsMenu( menu );
+    }
+
+    public void inicijalizacija() {
+        btnSearch = findViewById( R.id.btn_search );
+        movieName = findViewById( R.id.ime_filma );
+        recyclerView = findViewById( R.id.rvLista );
+
+        btnSearch.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getMovieByName( movieName.getText().toString() );
+            }
+        } );
+    }
+
+    private void getMovieByName(String name) {
     }
 }
